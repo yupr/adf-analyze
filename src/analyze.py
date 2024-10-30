ee60f8f02fc68c36699259027b7086dbdd56897c
yupr <12003186+yupr@users.noreply.github.com>
2024-10-30 16:27:46 +0900

    定常性の説明についてより正確な表現に修正

--- src/analyze.py
+++ src/analyze.py
@@ -20,11 +20,11 @@ def checkAdf(fileName: str, data: pd.DataFrame):
     # 原系列のp値を算出 (p値 == 単位根過程の確率)
     adf = adfuller(data)[1]
 
-    print(fileName, "のP値:", adf)
+    print(fileName, "のP値:", round(adf, 2))
     if adf <= 0.05:
         print("帰無仮説が棄却され、定常性があると言える。\n")
     else:
-        print("単位根過程であるため、非定常性であると言える。\n")
+        print("帰無仮説が棄却されず、定常性があるとは言えない。\n")
 
 
 # 増減率の算出
